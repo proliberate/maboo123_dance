aebfad19e818846cca5c6b89bf87fc7acfd44792
proliberate <andrew@proliberate.org>
2013-11-14 16:21:40 -0500

    initial azure commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dancestudio::Application.config.secret_key_base = '0f1ebd0f90eaa51019b2c0c0bc5525cfe9a691810e24738c12c08eb7116209e4a087cc85c6d7b1ae777d6adf419a85052780815ed6526c116fa72cf52b87d12f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Dancestudio::Application.config.session_store :cookie_store, key: '_dancestudio_session'
